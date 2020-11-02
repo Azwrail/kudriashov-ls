51ffd40ea41afa381ec53af5a01f83444fa90223
Oleg <ol.kudryashov@gmail.com>
2020-11-02 14:12:41 +0300

    Make Skills and Portfolio sections with Vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,43 @@
-console.log('this is the skills module');
+import Vue from "vue";
+
+const skillsItem = {
+    template: "#skill-item",
+    props: ["skill"],
+    methods: {
+        drawCircle() {
+            const circle = this.$refs["circle"];
+            const dashArray = parseInt(
+                getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+            );
+            const percent = (dashArray / 100) * (100 - this.skill.percent);
+            circle.style.strokeDashoffset = percent;
+        }
+    },
+    mounted() {
+        this.drawCircle()
+    }
+};
+
+const skillsRow = {
+    template: "#skills-row",
+    components: {
+        skillsItem
+    },
+    props: ["category"]
+};
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    components: {
+        skillsRow
+    },
+    data() {
+        return {
+            skills: []
+        }
+    },
+    created() {
+        this.skills = require("../data/skills.json");
+    }
+});
