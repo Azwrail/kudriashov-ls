6919e29b1556990187488e6284691f46760044ba
Oleg <ol.kudryashov@gmail.com>
2020-11-05 19:42:43 +0300

    Make review buttons disabled in end points

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -9,6 +9,8 @@ new Vue({
     data() {
         return {
             reviews: [],
+            isDisableNext: false,
+            isDisablePrev: false
         }
     },
     methods: {
@@ -28,11 +30,27 @@ new Vue({
                     document.querySelector('.VueCarousel-navigation-prev').click()
                     break;
             }
+        },
+        listenPageChange(pagenumber) {
+            const actualPageNumber = pagenumber +1;
+            if (this.maxPageNumber === actualPageNumber) {
+                this.isDisableNext = true;
+            } else if (actualPageNumber === 1) {
+                this.isDisablePrev = true;
+            } else {
+                this.isDisableNext = false;
+                this.isDisablePrev = false;
+            }
         }
+    },
+    computed: {
+      maxPageNumber() {
+          return this.reviews.length / 2
+      }
     },
     created() {
         this.reviews = require("../data/reviews.json");
         this.reviews = this.requireImagesToArray(this.reviews)
-
+        this.isDisablePrev = true;
     }
 })
